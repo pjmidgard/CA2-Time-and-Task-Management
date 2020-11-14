727bb7e2d74f4344911e50f27a8a70146ed19bb2
andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>
2020-11-14 16:56:24 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
 
-        System.out.println("Hello World!!");
+        System.out.println("Hello World! - Andre Goncalves Brazil" );
     }
-    
+     //Enter your command line bellow, so we can test out the shared file and edition, please! :)
 }
