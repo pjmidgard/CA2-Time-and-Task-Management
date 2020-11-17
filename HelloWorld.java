aee907dd8a2e7f5f82662c23be8bd53c75d6089f
lucasgustavo07 <72029920+lucasgustavo07@users.noreply.github.com>
2020-11-17 12:38:20 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! - Andre Goncalves, Brazil" );
+        System.out.println("Hello World! - Andre Goncalves, Brazil", "Hello World! Lucas Gustavo, Brazil");
     }
      //Enter your command line bellow, so we can test out the shared file and edition, please! :)
 }
