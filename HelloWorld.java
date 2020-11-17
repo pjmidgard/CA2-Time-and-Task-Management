e08e6233b8418d8a8fb400523cd5691f0e2cb431
Gantogtokh <b.ganaa592@gmail.com>
2020-11-17 15:30:43 +0000

    Update HelloWorld.java
    Co-Authored-By: andreccgoncalves <72031101+andreccgoncalves@users.noreply.github.com>

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World! - Andre Goncalves, Brazil", "Hello World! Lucas Gustavo, Brazil");
+        System.out.println("Hello World! Gantogtokh, Mongolia");// test
     }
      //Enter your command line bellow, so we can test out the shared file and edition, please! :)
 }
